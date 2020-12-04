9b7beb8b71caf0f72e43233226c1ccd3deae6b19
Cathal Donohoe <cdonohoe1374@gmail.com>
2020-12-04 21:04:47 +0000

    Added menu system, new player, animations, sound, audiocontroller

---
+++ Assets/Scripts/Events/Events.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Events : MonoBehaviour
+{
+    public void ReplayGame(){
+        FindObjectOfType<AudioManager>().Playsound("Replay");
+        ScoreScript.scoreValue = 0;
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame(){
+        FindObjectOfType<AudioManager>().Playsound("Quit");
+        Application.Quit();
+    }
+}
---
+++ Assets/Scripts/Functions/MenuScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    public void PlayGame(){
+        FindObjectOfType<AudioManager>().Playsound("Play");
+        SceneManager.LoadScene("Level");
+    }
+
+    public void QuitGame(){
+        FindObjectOfType<AudioManager>().Playsound("Quit");
+        Application.Quit();
+    }
+}
